7ec2c45dcd09704da8095376569da31b41aae0ff
hotdog929 <hotdog929@gmail.com>
2019-06-28 16:01:34 +0800

    helloSvc2 test

--- app/src/main/kotlin/demo/submodule/svc1/app/App.kt
+++ app/src/main/kotlin/demo/submodule/svc1/app/App.kt
@@ -8,8 +8,14 @@ class App {
         println(svc1.hello("SVC1"))
     }
 
+    val svc2: demo.submodule.svc1.LibrarySvc2 = demo.submodule.svc1.LibrarySvc2()
+
+    fun helloSvc2(){
+        println(svc2.hello("SVC2"))
+    }
 }
 
 fun main(args: Array<String>) {
     App().hello()
+    App().helloSvc2()
 }
